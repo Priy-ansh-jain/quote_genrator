2e19c1178ac4e862b844ceb6c91035d1b100c868
Priyansh Jain <priyanshjain303@gmail.com>
2023-10-28 18:19:20 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+class App extends React.Component {
+  state = { advice: "" };
+  //  recycl method
+  componentDidMount() {
+    this.fetchAdvice();
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // In a class component in React, methods that you want to use as part of the component's behavior, especially lifecycle methods like componentDidMount, should not be declared using const. Instead, they should be declared as class methods.
+  fetchAdvice = () => {
+    // // get method OR get Request = > to request the random api
+    axios
+      .get("https://api.adviceslip.com/advice")
+      //  to get response
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice }); // we use this to access advice in outer scope using state.
+        // we have set advice here.
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    const { advice } = this.state;
+    return (
+      
+      <div className="app">
+
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+        <div>
+          <div id="ball"></div>
+          <div id="ball2"></div>
+          <div id="ball3"></div>
+          <div id="ball4"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
